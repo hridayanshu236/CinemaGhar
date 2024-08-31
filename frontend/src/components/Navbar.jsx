66ee2cbe1f45ca9431ddba5a9ea43b4ad09b3a08
Hridayanshu Raj Acharya <148516090+hridayanshu236@users.noreply.github.com>
2024-08-31 21:37:44 +0545

    improved responsiveness on small screen devices

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ const Navbar = () => {
             <nav className={`w-full flex md:px-10 justify-between items-center text-white bg-black py-[15px] px-4 border-y border-gray-700 transition-transform duration-500 ${showNavbar ? 'translate-y-0' : '-translate-y-full'}`}>
                 <div className='flex items-center'>
                     <img className='w-[60px] h-[60px] cursor-pointer' src={logo} alt="Logo" />
-                    <div className='flex'>
+                    <div className='flex p-3 pt-5'>
                         <p className='font-semibold text-lg'><span className='text-purple-500'>Cinema</span></p>
                         <p className='pt-0.5 pl-1'>Ghar</p>
                     </div>
@@ -118,39 +118,39 @@ const Navbar = () => {
                     )}
                 </ul>
             </nav>
-            <div className={`fixed top-0 left-0 w-[250px] h-full bg-black bg-opacity-50 text-white z-50 transition-transform duration-500 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-                <div className='flex justify-between items-center p-4'>
+            <div className={`fixed top-0 left-0 w-[250px] h-[100vh] bg-black bg-opacity-50 text-white z-50 transition-transform duration-500 ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+                {/* <div className='flex justify-between items-center p-4'>
                     <FontAwesomeIcon icon={faX} onClick={toggleSidebar} className='cursor-pointer' />
-                </div>
-                <ul className='mt-10'>
-                    <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                </div> */}
+                <ul className='mt-14'>
+                    <li className=' my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                         <NavLink to="/" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                             Home
                         </NavLink>
                     </li>
-                    <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                    <li className=' my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                         <NavLink to="/movies" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                             Movies
                         </NavLink>
                     </li>
-                    <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                    <li className=' my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                         <NavLink to="/rates" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                             Rates
                         </NavLink>
                     </li>
-                    <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                    <li className='my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                         <NavLink to="/about" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                             About
                         </NavLink>
                     </li>
                     {!isLoggedIn ? (
                         <>
-                            <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                            <li className=' my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                                 <NavLink to="/login" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                                     Login
                                 </NavLink>
                             </li>
-                            <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                            <li className='my-[10px] mx-[20px] text-lg cursor-pointer p-3'>
                                 <NavLink to="/signup" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                                     Signup
                                 </NavLink>
@@ -158,12 +158,12 @@ const Navbar = () => {
                         </>
                     ) : (
                         <>
-                            <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                            <li className='my-[10px] mx-[20px] text-lg cursor-pointer'>
                                 <NavLink to="/profile" className={({ isActive }) => isActive ? "text-purple-500 sm:bg-transparent" : "text-white"}>
                                     <FontAwesomeIcon icon={faUser} />
                                 </NavLink>
                             </li>
-                            <li className='inline-block my-[10px] mx-[20px] text-lg cursor-pointer'>
+                            <li className='my-[10px] mx-[20px] text-lg cursor-pointer'>
                                 <FontAwesomeIcon icon={faSignOut} onClick={handleLogout} className="cursor-pointer" />
                             </li>
                         </>
